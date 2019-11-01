b1f4765e59ce8a46ed79d035e7edd26f81bffec5
qianxinfeng <qianxinfeng@meituan.com>
2019-11-01 13:09:23 +0800

    update by publish:Fri Nov  1 13:09:23 CST 2019

--- bin/publish.sh
+++ bin/publish.sh
@@ -4,9 +4,19 @@ set -e
 
 THIS_DIR="$(cd "$(if [[ "${0:0:1}" == "/" ]]; then echo "$(dirname $0)";else echo "$PWD/$(dirname $0)";fi)"; pwd)"
 PROJECT_DIR="${THIS_DIR}/.."
+THEME_PROJECT_DIR="${PROJECT_DIR}/themes/hugo-nuo"
+
+function commit_theme(){
+    pushd $THEME_PROJECT_DIR
+      git add .
+      git commit -m "commit by publish `date`"
+      git push
+    popd
+}
 
 function main(){
   echo -e "\033[32m publish to github... \033[0m"
+  commit_theme
   pushd $PROJECT_DIR
    git add .
    git commit -m "update by publish:`date`"
